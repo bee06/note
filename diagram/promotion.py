f61d3bc92a6ddd98fd1e26225b01ef02c590fd8c
bee06 <zong.jianhui06@gmail.com>
2023-02-27 18:50:47 +0800

    add c4

--- diagram/promotion.py
+++ diagram/promotion.py
@@ -1,8 +1,8 @@
-from diagrams import Diagram
+from diagrams import  Cluster, Diagram, Edge
 from diagrams.aws.compute import EC2
 from diagrams.aws.database import RDS
 from diagrams.aws.network import ELB
 import diagrams.programming.flowchart as FC
 
 with Diagram("promotion", show=False,outformat=["png"],direction="TB"):
-    FC.Action("Action") >> [FC.Database("Database")]
+    FC.Action("Action") >> Edge(label="collect",color="brown", style="dotted") >> [FC.Database("Database")]
