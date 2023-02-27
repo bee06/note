646c381f9084446be6aa4f7472b8e0258cfbd861
bee06 <zong.jianhui06@gmail.com>
2023-02-27 18:03:20 +0800

    增加促销

---
+++ diagram/promotion.py
@@ -0,0 +1,8 @@
+from diagrams import Diagram
+from diagrams.aws.compute import EC2
+from diagrams.aws.database import RDS
+from diagrams.aws.network import ELB
+from diagrams.programming.flowchart import _Flowchart
+
+with Diagram("promotion", show=False):
+    
